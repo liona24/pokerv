d8c92e5ccc777e34b74d15ce329d77a1f620d4a6
Lion Ackermann <ackerm.lion@hotmail.de>
2018-09-13 12:32:41 +0200

    Simplify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import socketio from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io';
 
-export const SocketInstance = socketio('http://localhost:5000');
-
-Vue.use(VueSocketIO, SocketInstance);
+Vue.use(VueSocketIO, 'http://localhost:5000');
 
 Vue.config.productionTip = false
 
